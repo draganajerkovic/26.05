091a96d25ed709f0ee4caf640b8667d6531a0dcf
Dragana <jerkovicdragana.jd@gmail.com>
2022-05-28 22:41:15 +0200

    test

--- domaci2605/src/main/java/Main.java
+++ domaci2605/src/main/java/Main.java
@@ -1,5 +1,47 @@
+/*
+Otići na kupujemprodajem.com,
+u pretragu ukucati iphone 13, kliknuti na prvi rezultat. Proveriti
+ da je naslov oglasa onaj koji smo uočili i tokom manuelnog izvođenja kejsa.
+ */
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Korisnik\\Desktop\\IT bootcamp\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.kupujemprodajem.com/");
+        driver.manage().window().maximize();
 
+        WebElement klikniSaStrane=driver.findElement(By.xpath("//*[@id=\"bodyTag\"]/div[9]/div/div[2]"));
+        klikniSaStrane.click();
+        WebElement searchPolje=driver.findElement(By.xpath("//*[@id=\"searchKeywordsInput\"]"));
+        searchPolje.sendKeys("iphone 13");
+        searchPolje.sendKeys(Keys.ENTER);
+        WebElement prazno=driver.findElement(By.xpath("//*[@id=\"bodyTag\"]"));
+        prazno.sendKeys(Keys.ARROW_DOWN);
+        prazno.sendKeys(Keys.ARROW_DOWN);
+        prazno.sendKeys(Keys.ARROW_DOWN);
+        prazno.sendKeys(Keys.ARROW_DOWN);
+        prazno.sendKeys(Keys.ARROW_DOWN);
+        prazno.sendKeys(Keys.ARROW_DOWN);
+        WebElement prviOglas=driver.findElement(By.xpath("//*[@id=\"adDescription7321776\"]/div/section[2]/div[1]/div[1]/a"));
+        prviOglas.click();
+        WebElement tekstOglasa=driver.findElement(By.xpath("//*[@id=\'oglas-holder\']/section/div[1]/h1"));
+       String tekst=tekstOglasa.getText();
+       String expected="iPhone 13, iPhone 12, iPhone 11 - NOVO!";
+       if(tekst.equals(expected)){
+           System.out.println("validno!");
+       }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
